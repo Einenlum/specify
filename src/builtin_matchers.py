d6b78594f8aad12a715f2b594943f22fdb120149
Einenlum <yann.rabiller@gmail.com>
2019-02-12 13:45:13 +0100

    Add more matchers

--- src/builtin_matchers.py
+++ src/builtin_matchers.py
@@ -10,10 +10,36 @@ def be_like(value, expected):
 
     return value
 
+def not_be(value, not_expected):
+    if value is not_expected:
+        raise Exception(f"Expected {value} not to be {not_expected}")
+
+    return value
+
+def not_be_like(value, not_expected):
+    if value == not_expected:
+        raise Exception(f"Expected {value} not to be like {not_expected}")
+
+    return value
+
+def be_an_instance_of(value, cls):
+    if not isinstance(value, cls):
+        raise Exception(f"Expected {value} to be an instance of {cls.__qualname__}, got {type(value)}")
+
+    return value
+
 def get_matcher(type):
     items = {
         'be': be,
-        'be_like': be_like
+        'not_be': not_be,
+        'return': be,
+        'not_return': not_be,
+        'be_like': be_like,
+        'not_be_like': not_be_like,
+        'return_like': be_like,
+        'not_return_like': not_be_like,
+        'be_an_instance_of': be_an_instance_of,
+        'return_an_instance_of': be_an_instance_of
     }
 
     if type not in items:
